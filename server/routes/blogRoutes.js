b89d97c6c53198150fe230b5c274fe4cd6f342fa
Kyd <kyd.kitchaiya@medieinstitutet.se>
2021-04-16 10:18:41 +0200

    cleaned up files

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const blogController = require("../controllers/blogController")
 
 router.get("", blogController.getAllBlogs); 
-router.get("" + "/:id", blogController.getBlogById); 
+router.get("/:id", blogController.getBlogById); 
 router.post("", blogController.postBlog);
-router.put("" + "/:id", blogController.editBlog); 
-router.delete("" + "/:id", blogController.deleteBlog)
+router.put("/:id", blogController.editBlog); 
+router.delete("/:id", blogController.deleteBlog)
 
 module.exports = router; 
